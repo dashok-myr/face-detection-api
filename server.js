1bf1b39546bf2bda38080b434be877297f5ca6ed
Daria Usatyuk <dashok.myr@gmail.com>
2020-03-19 15:34:06 -0700

    adding database connection

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-rectangular-81143",
-    user: "dariausatyuk",
-    password: "",
-    database: "smart-brain"
+    host: "process.env.DATABASE_URL",
+    ssl: true
   }
 });
 
