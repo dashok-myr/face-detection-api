b78627362c3dd80354769fb00e794dd28da992e3
Daria Usatyuk <dashok.myr@gmail.com>
2020-03-18 18:52:57 -0700

    Created face-detection-apt

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+const knex = require("knex");
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "dariausatyuk",
+    password: "",
+    database: "smart-brain"
+  }
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
+
+//signin --> POST = success/fail
+app.post("/signin", (req, res) => { signin.handelSignin(req, res, db, bcrypt) })
+//register --> POST = user
+app.post("/register", (req, res) => { register.handleRegister(req, res, db, bcrypt) }) 
+//profile/: PUT --> user
+app.get("/profile/:id", (req, res) => { profile.handleProfileGet(req, res, db) })
+//image --> PUT --> user
+app.put("/image", (req, res) => { image.handleImage(req, res, db) })
+app.post("/imageurl", (req, res) => { image.handleApiCall(req, res) })
+
+app.listen(3000, () => {
+  console.log("app is running on post 3000");
+});
+
